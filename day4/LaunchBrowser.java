120ac804a35c1da26865465e12627aaf2fe6333c
Bharath-K-G <137047144+Bharath-K-G@users.noreply.github.com>
2023-06-22 15:51:00 +0530

    Add files via upload

---
+++ day4/LaunchBrowser.java
@@ -0,0 +1,29 @@
+package week1.day4;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class LaunchBrowser {
+public static void main(String[] args) throws InterruptedException {
+	
+	ChromeDriver driver = new ChromeDriver();
+	driver.manage().window().maximize();
+	driver.get("https://login.salesforce.com/");
+	
+	Thread.sleep(2000);
+	
+	driver.findElement(By.id("username")).sendKeys("hari.radhakrishnan@qeagle.com");
+	Thread.sleep(3000);
+	driver.findElement(By.id("password")).sendKeys("Leaf@1234");
+	Thread.sleep(3000);
+	driver.findElement(By.id("Login")).click();
+	Thread.sleep(5000);
+	
+	
+	
+	driver.close();
+	
+	
+	
+}
+}
---
+++ day4/TestTwo.java
@@ -0,0 +1,45 @@
+package week1.day4;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class TestTwo {
+
+	public static void main(String[] args) throws InterruptedException {
+		ChromeDriver driver=new ChromeDriver();
+		driver.get("http://leaftaps.com/opentaps/control/login");
+		driver.manage().window().maximize();
+		Thread.sleep(2000);
+		driver.findElement(By.id("username")).sendKeys("DemoSalesManager");
+		driver.findElement(By.id("password")).sendKeys("crmsfa");
+		Thread.sleep(2000);
+		driver.findElement(By.className("decorativeSubmit")).click();
+		Thread.sleep(2000);
+		driver.findElement(By.linkText("CRM/SFA")).click();
+		Thread.sleep(2000);
+		driver.findElement(By.linkText("Leads")).click();
+		Thread.sleep(2000);
+		driver.findElement(By.linkText("Create Lead")).click();
+		Thread.sleep(2000);
+		driver.findElement(By.id("createLeadForm_companyName")).sendKeys("TestLeaf");
+		Thread.sleep(2000);
+		driver.findElement(By.id("createLeadForm_firstName")).sendKeys("Bharath");
+		Thread.sleep(2000);
+		driver.findElement(By.id("createLeadForm_lastName")).sendKeys("Kumar");
+		Thread.sleep(2000);
+		driver.findElement(By.name("submitButton")).click();
+		Thread.sleep(2000);
+		driver.findElement(By.linkText("Edit")).click();
+		Thread.sleep(2000);
+		driver.findElement(By.className("inputBox")).clear();
+		driver.findElement(By.className("inputBox")).sendKeys("TCS");
+		Thread.sleep(2000);
+		driver.findElement(By.name("submitButton")).click();
+		Thread.sleep(2000);
+		
+		driver.close();
+		
+
+	}
+
+}
